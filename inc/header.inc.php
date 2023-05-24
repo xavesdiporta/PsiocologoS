0970bac05a84013c15ff90c879d3c086f108fdb9
Xaves <86982363+T3nco@users.noreply.github.com>
2023-05-24 09:40:41 +0100

    Login and Sign Up Form
    Foi adicionado o Login Form e o Sign Up Form's, com o seu CSS;

---
+++ inc/header.inc.php
@@ -0,0 +1,23 @@
+<header>
+    <title>Psicologa para burros</title>
+    <link rel="stylesheet" href="../Final_One/inc/main.inc.css">
+</header>
+
+
+<body>
+    <nav>
+        <a class="navbar-brand" href="#">
+            <img src="../Final_One/img/logo.jpg" alt="Logo" style="width:40px;">
+         </a>
+        <div class="nav-links">
+            <ul>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contactos</a></li>
+            </ul>
+        </div>
+        <form>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </nav>
+</body>

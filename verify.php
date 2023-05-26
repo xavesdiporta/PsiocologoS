995a1299ea2f540e0cc2abdce5115f6c9abf220d
QuaNilo <quanojo@gmail.com>
2023-05-26 11:18:46 +0100

    Added authentication variation Xavier, até que ponto é que o $_SESSION['authentication'] não devia de ser $_COOKIE?

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 // Inicia sessões 
 session_start(); 
 // Verifica se existe os dados da sessão de login 
-if(!isset($_SESSION["authenticated"])) { 
+if(!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] == false) { 
 // Usuário não logado! Redireciona para a página de login 
 header("Location: login.html"); 
 exit; 

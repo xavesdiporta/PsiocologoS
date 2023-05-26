0050aed7e5d3def10c8baddc330fe7a2950ce037
QuaNilo <quanojo@gmail.com>
2023-05-26 10:48:47 +0100

    implementar logins rudimentar

--- login.php
+++ login.php
@@ -1,44 +1,20 @@
-<!DOCTYPE html>
-<html>
+<?php
+include 'connect.php';
 
-     
-    <!-- head things -->
-    <head>
-        <meta charset="utf-8" />
-        <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Psicologo da Mosca - Login</title>
-        <link rel = "stylesheet" href = "inc/main.inc.css">
-        <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    </head>
+session_start();
+    $stmt = mysqli_prepare($con, "SELECT password FROM users WHERE username = ?");
+      mysqli_stmt_bind_param($stmt, "s", $username);
+      mysqli_execute($stmt);
+      mysqli_stmt_bind_result($stmt, $hashedPassword);
+      mysqli_stmt_fetch($stmt);
+      if(password_verify($password, $hashedPassword)){
+        echo "Login complete";
+        $_SESSION['authenticated'] = true;
+        header("Location: users.php");
+      }
+      else{
+        echo "Error logging in  ";
+        header("Location: login.html");
+      }
 
-
-    <!-- body things -->
-    <body>
-
-        <div class = "wrapper">
-            <section class = "form login">
-                <header>Realtime Chat App</header>
-                <form action = "#" method = "post">
-                    <div class = "error-txt">This is an error message!</div>
-                    <div class = "field input">
-                        <label>Email Address</label>
-                        <input type = "text" name = "email" placeholder = "Enter your email" required>
-                    </div>
-                    <div class = "field input">
-                        <label>Password</label>
-                        <input type = "password" name = "password" placeholder = "Enter new password" required>
-                        <i class = "fas fa-eye"></i>
-                    </div>
-                    <div class = "field button">
-                        <input type = "submit" value = "Continue to Chat">
-                    </div>
-                </form>
-                <div class = "link">Not yet signed up? <a href = "#">Sign Up now</a></div>
-            </section>
-        </div>
-
-        <script src = "inc/pass-show-hide.js"></script>
-
-    </body>
-</html>
+?>

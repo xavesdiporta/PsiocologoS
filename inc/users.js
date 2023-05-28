363ba85786163f27751221afe3abe3e6f4cc4a9f
QuaNilo <quanojo@gmail.com>
2023-05-28 18:52:55 +0100

    Passed status as a parameter aswell

--- inc/users.js
+++ inc/users.js
@@ -13,17 +13,17 @@ searchBtn.onclick = ()=>{
 function openChat(user){
         // Create an iframe element
         var iframe = document.createElement('iframe');
-        iframe.src = 'chat.php?username=' + user;
+        iframe.src = 'chat.php?username=' + user + "&status=Active";
         iframe.style.width = '600px';
         iframe.style.height = '100%';
         iframe.style.border = 'none';
         iframe.style.marginLeft = '20px'; // Define uma margem esquerda de 20 pixels
 
-    
+
         // Remove any existing chat content
         var chatContent = document.getElementById('chatContent');
         chatContent.innerHTML = '';
-    
+
         // Append the iframe to the chatContent div
         chatContent.appendChild(iframe);
 }

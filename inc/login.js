0d18e2c0894494c7132d69a945a9a9e0392bf332
QuaNilo <quanojo@gmail.com>
2023-06-01 10:01:43 +0100

    fixed normal user not logging in

--- inc/login.js
+++ inc/login.js
@@ -1,3 +1,6 @@
-document.getElementById('anonymous').onclick() = function (){
-    
+document.getElementById('anonymous').onclick = function (){
+    window.location.href = 'login.php';
+    // Change the "required" attribute of the input field
+    var inputUsername = document.getElementsByName('username')[0];
+    inputUsername.removeAttribute('required');
 }
--- php/login.php
+++ php/login.php
@@ -1,37 +1,39 @@
 <?php
-
+session_abort();
 session_start();
 include_once '../inc/connect.php';
 
-$username = mysqli_real_escape_string($con, $_POST['username']);
-$password = mysqli_real_escape_string($con, $_POST['password']);
 
-if (!empty($username || isset($_POST['anonymous']))) {
+if (!empty($_POST['username']) || isset($_POST['anonymous'])) {
     if (isset($_POST['anonymous'])) {
         // Handle login as anonymous user
-
+        
         //get data from json file
         $jsonData = file_get_contents('randomNames.json');
         //convert data to PHP array
         $data = json_decode($jsonData, true);
         $lengthData = count($data['myArray']);
         $randIndex = rand(0,$lengthData);
-
+        
         $_SESSION['authenticated'] = true;
         $_SESSION['mainUserName'] = $data['myArray'][$randIndex]['name'];
+        $_SESSION['mainUserPassword'] = $data['myArray'][$randIndex]['password'];
         $_SESSION['mainUserStatus'] = 'Active';
         header("Location: users.php");
         exit;
     } else {
         // Handle login with username and password
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $username = mysqli_real_escape_string($con, $_POST['username']);
         if (!empty($password)) {
+            
             $stmt = mysqli_prepare($con, "SELECT password FROM users WHERE username = ?");
             mysqli_stmt_bind_param($stmt, "s", $username);
             mysqli_execute($stmt);
             mysqli_stmt_bind_result($stmt, $hashedPassword);
             mysqli_stmt_fetch($stmt);
             if (password_verify($password, $hashedPassword)) {
-                echo "Login complete";
+                //password verification sucessful
                 $_SESSION['authenticated'] = true;
                 $_SESSION['mainUserName'] = $username;
                 $_SESSION['mainUserStatus'] = "Active";
@@ -41,17 +43,20 @@ if (!empty($username || isset($_POST['anonymous']))) {
                 header("Location: users.php");
                 exit;
             } else {
+                //incorrect password
                 include 'login.html';
                 echo "<p style='font-size: 17px;font-weight: 400;border-radius: 5px; border: 2px solid black; width: 175px; height: 50px; color: white; background : red; text-align: center;padding-top: 12px; margin-bottom: 30px;'>ERROR LOGGING IN</p>";
                 exit;
             }
         } else {
+            //password is empty
             include 'login.html';
             echo "<p style='font-size: 17px;font-weight: 400;border-radius: 5px; border: 2px solid black; width: 175px; height: 50px; color: white; background : red; text-align: center;padding-top: 12px; margin-bottom: 30px;'>Password is required</p>";
             exit;
         }
     }
 } else {
+    //username is empty
     include 'login.html';
     echo "<p style='font-size: 17px;font-weight: 400;border-radius: 5px; border: 2px solid black; width: 175px; height: 50px; color: white; background : red; text-align: center;padding-top: 12px; margin-bottom: 30px;'>Username is required</p>";
     exit;

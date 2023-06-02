689a1b63d113fe079473b8522df86731eb45c938
Xaves <86982363+T3nco@users.noreply.github.com>
2023-06-02 09:53:21 +0100

    css configs

--- php/verify.php
+++ php/verify.php
@@ -2,9 +2,8 @@
 // Inicia sessões 
 session_start(); 
 // Verifica se existe os dados da sessão de login 
-if(!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] == false) { 
+if(!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] == false || empty($_SESSION['mainUserName'])) { 
 // Usuário não logado! Redireciona para a página de login 
 header("Location: login.html"); 
-exit; 
 } 
 ?> 
